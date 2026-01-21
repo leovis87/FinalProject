72c8005139d86fe859d3e9dcc91045377d5cc5b2
faust <leejaechul92@gmail.com>
2026-01-21 16:02:48 +0900

    core/config.py 에러 해결

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -44,6 +44,7 @@ class Settings(BaseSettings):
 
         env_file = os.path.join(backend_dir, ".env")
         env_file_encoding = "utf-8"
+        extra = "ignore"
 
 @lru_cache()
 def get_settings() -> Settings:
